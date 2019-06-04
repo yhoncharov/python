66ea2ad2e4a218b282228ad2fd6ce33681257533
yhoncharov <34937859+yhoncharov@users.noreply.github.com>
2019-06-04 17:29:18 +0300

    Create dz5.py

---
+++ dz5.py
@@ -0,0 +1,8 @@
+def is_divisible(m, n):
+    if m == 0 or n == 0: return print ("m or n  = 0")
+    elif m%n==0: print("True")        
+    else: print("False") 
+
+# What is returned here?
+is_divisible(3, 10)
+
